b0518801857b0c5d7c465448642324b2cf6362c3
Pethanan <105306532+Pethanan@users.noreply.github.com>
2023-01-28 22:30:45 +0530

    Components - organized

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,3 +1,5 @@
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props)
@@ -5,15 +7,9 @@ function ExpenseItem(props)
 
     return(
         <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
-            <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className="expense-item__price">INR.{props.amount}</div>
-                                <div className="expense-item__price">{props.location}</div>
-
-            </div>
-        
-            </div>
+            <ExpenseDate date={props.date}/>
+            <ExpenseDetails title={props.title} amount={props.amount} location={props.location}/>
+        </div>
     );
 }
 
