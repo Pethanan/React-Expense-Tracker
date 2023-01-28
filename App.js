67bc3d60b97a3a54dd78ffabe6abcebb16a9d0d2
Pethanan <105306532+Pethanan@users.noreply.github.com>
2023-01-28 16:09:10 +0530

    Project - starting point

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div>
+      <h1> Expense Item   </h1>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+       </div>
+  );
+}
+
+export default App;
---
+++ ExpenseItem.js
@@ -0,0 +1,18 @@
+function ExpenseItem()
+{
+    return(
+        <div>
+            <div>date</div>
+            <div>Food</div>
+            <div>Rs. 10</div>
+            <div>date</div>
+            <div>Petrol</div>
+            <div>Rs. 100</div>
+            <div>date</div>
+            <div>Movie</div>
+            <div>Rs.200</div>
+            </div>
+    );
+}
+
+export default ExpenseItem;
