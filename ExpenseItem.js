b4a94eb276b33f8f69ed11c3e0e26b0634304256
Pethanan <105306532+Pethanan@users.noreply.github.com>
2023-01-30 22:37:23 +0530

    Delete button added

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -5,10 +5,15 @@ import "./ExpenseItem.css";
 const ExpenseItem = (props) =>
 {
 
+    const clickHandler = () => {
+
+    }
+
     return(
         <div className="expense-item">
             <ExpenseDate date={props.date}/>
             <ExpenseDetails title={props.title} amount={props.amount} location={props.location}/>
+            <button onClick={clickHandler}>Delete</button>
         </div>
     );
 }
