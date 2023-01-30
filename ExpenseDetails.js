f1b3715abf38688897c89e2af2eec68eff324eca
Pethanan <105306532+Pethanan@users.noreply.github.com>
2023-01-31 00:27:27 +0530

    useState() added for amount and title

--- ExpenseDetails.js
+++ ExpenseDetails.js
@@ -1,13 +1,18 @@
 
+import {useState} from "react";
 import "./ExpenseDetails.css"
 const ExpenseDetails = (props) =>
 
 {
 
+    const [amount, setAmount] = useState(props.amount);
+   const clickHandler = () => {
+    setAmount(100);
+   }
     return (
         <div className="expense-item__description">
                 <h2>{props.title}</h2>
-                <div className="expense-item__price">INR {props.amount}</div>
+                <div className="expense-item__price">INR {amount}</div> <button onClick={clickHandler}>Edit Expense amnt</button>
                                 <div className="expense-item__price">{props.location}</div>
 
             </div>
--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,3 +1,4 @@
+import {useState} from "react";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import "./ExpenseItem.css";
@@ -5,15 +6,18 @@ import "./ExpenseItem.css";
 const ExpenseItem = (props) =>
 {
 
+    const [title, setTitle] = useState(props.title);
     const clickHandler = () => {
+        setTitle("updated");
+        console.log(title)
 
     }
 
     return(
         <div className="expense-item">
             <ExpenseDate date={props.date}/>
-            <ExpenseDetails title={props.title} amount={props.amount} location={props.location}/>
-            <button onClick={clickHandler}>Delete</button>
+            <ExpenseDetails title={title} amount={props.amount} location={props.location}/>
+            <button onClick={clickHandler}>Edit</button>
         </div>
     );
 }
