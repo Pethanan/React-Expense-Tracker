3b8a1699fdff0d7875b772b9769245a97751be76
Pethanan <105306532+Pethanan@users.noreply.github.com>
2023-01-31 23:50:03 +0530

    onChange - form event added

--- ExpenseForm.js
+++ ExpenseForm.js
@@ -2,9 +2,13 @@ import React from "react";
 import "./ExpenseForm.css"
 
 const ExpenseForm = () => {
+const changeHandler = () => {
 
+    console.log("input form clicked")
+
+};
     return (
-        <form className="frm">
+        <form className="frm" onChange={changeHandler}>
             <label className="label">Expense Title</label>
             <input type="text" className="input"></input>
             <label className="label">Expense Amount</label>
@@ -14,6 +18,6 @@ const ExpenseForm = () => {
             <button type="submit" className="btn">Submit</button>
         </form>
     );
-}
+};
 
 export default ExpenseForm;
