6e9c6cefb7d55e1907c1223ddb964b109e3bfabd
Pethanan <105306532+Pethanan@users.noreply.github.com>
2023-02-04 23:20:54 +0530

    filter applied to expenses

--- Expenses.js
+++ Expenses.js
@@ -10,8 +10,8 @@ const Expenses = (props) => {
 
 
 
-      const [filteredYear, setFilteredYear] = useState("");
-
+      const [filteredYear, setFilteredYear] = useState("2023");
+      const filteredExpenses = props.expenses.filter(el => el.date.getFullYear().toString() === filteredYear);
       const filterChangeHandler = selectedYear => {
 
             setFilteredYear(selectedYear);
@@ -22,7 +22,7 @@ const Expenses = (props) => {
             <div>
                   <ExpensesFilter selected={filteredYear} onFilterChangeHandler={filterChangeHandler}></ExpensesFilter>
                   <div className="expenses">
-                        {props.expenses.map((el) => <ExpenseItem key={el.id} location={el.location} title={el.title} amount={el.amount} date={el.date} ></ExpenseItem>)}</div>
+                        {filteredExpenses.map((el) => <ExpenseItem key={el.id} location={el.location} title={el.title} amount={el.amount} date={el.date} ></ExpenseItem>)}</div>
             </div>);
 
 };
